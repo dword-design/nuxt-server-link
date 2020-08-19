0c2508b75ff3db05dcbc1bea5b27493b379f856c
foo <foo@bar.de>
2020-08-19 14:24:07 +0200

    fix: fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import pushPlugins from '@dword-design/nuxt-push-plugins'
-import P from 'path'
 
 export default function () {
-  this.addTemplate({
-    fileName: P.join('nuxt-server-link', 'component.vue'),
-    src: require.resolve('./component.vue'),
-  })
-  pushPlugins(this, {
-    fileName: P.join('nuxt-server-link', 'plugin.js'),
-    src: require.resolve('./plugin'),
-  })
+  pushPlugins(this, require.resolve('./plugin'))
 }
